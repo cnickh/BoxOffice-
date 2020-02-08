2a21a61061af842de01f2950fb9d179163f8dfa0
chrisbefin <christian@jba4.com>
2020-02-08 13:18:41 -0700

    printed out equation

--- readData.py
+++ readData.py
@@ -19,35 +19,37 @@ df.dropna(inplace=True)
 df['budget'] = pd.to_numeric(df['budget'])
 df['id'] = pd.to_numeric(df['id'])
 df['belongs_to_collection'] = pd.to_numeric(df['belongs_to_collection'])
-df = df[df.revenue != 0]
-df['budget:revenue'] = df['budget'] / df['revenue']
-df.dropna(inplace=True)
+# df = df[df.revenue > 0]
+#df = df[df.budget > 0]
+# df['budget:revenue'] = df['budget'] / df['revenue']
+# df.dropna(inplace=True)
 print(len(df))
-Y = pd.DataFrame(df['budget:revenue'])
-X = pd.DataFrame(df[['budget','runtime','vote_average','vote_count','belongs_to_collection']])
 
 def LinearRegression(X,Y):
     from sklearn.model_selection import train_test_split
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=5)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.5, random_state=5)
 
     from sklearn.linear_model import LinearRegression
-    regressor = LinearRegression(copy_X=True, fit_intercept=True, n_jobs=None, normalize=False)
+    regressor = LinearRegression(copy_X=True, fit_intercept=False, n_jobs=None, normalize=True)
     regressor.fit(X_train, Y_train)
+
+    print(regressor.intercept_)
 
     v = pd.DataFrame(regressor.coef_,index=['Co-efficient']).transpose()
     w = pd.DataFrame(X.columns, columns=['Attribute'])
 
     coeff_df = pd.concat([w,v],axis=1, join='inner')
-    print(coeff_df)
 
     Y_pred = regressor.predict(X_test)
     Y_pred = pd.DataFrame(Y_pred, columns=['Predicted'])
-    print(Y_pred)
+
     from sklearn import metrics
     print("Mean Absolute Error: {}" .format(metrics.mean_absolute_error(Y_test, Y_pred)))
     return coeff_df
 # print("Mean Squared Error: {}" .format(metrics.mean_squared_error(Y_test, Y_pred)))
 # print("Root Mean Squared Error: {}" .format(np.sqrt(metrics.mean_squared_error(Y_test, Y_pred))))
-Y = pd.DataFrame(df['budget:revenue'])
+Y = pd.DataFrame(df['revenue'])
 X = pd.DataFrame(df[['budget','runtime','vote_average','vote_count','belongs_to_collection']])
-LinearRegression(X,Y)
+result = LinearRegression(X,Y)
+
+print("Revenue = {}*budget {}*runtime {}*vote_average +{}*vote_count +{}*belongs_to_collection" .format(result.at[0,'Co-efficient'],result.at[1,'Co-efficient'],result.at[2,'Co-efficient'],result.at[3,'Co-efficient'],result.at[4,'Co-efficient']))
